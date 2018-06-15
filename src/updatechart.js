8f0600010bd6fbff75b12fabc31544b62859e174
Nikotest9 <frohlik1@web.de>
2018-06-15 08:52:32 +0200

    15.06 Zu Hause

--- src/updatechart.js
+++ src/updatechart.js
@@ -2,7 +2,9 @@ const container = d3.select('#scrolly-side');
 const stepSel = container.selectAll('.step');
 const imgSel = container.selectAll('.images');
 export function updateChart(index) {
-  const sel = container.select(`[data-index='${index}']`);
+  // const sel = container.select(`[data-index='${index}']`);
   stepSel.classed('is-active', (d, i) => i === index);
-  imgSel.classed('is-active', (d, i) => i === index);
+
+  // imgSel.classed('is-active', (d, i) => i === index);
+
 }
